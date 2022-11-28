e12d7e42eb7beeaa42c46c490a45b37e0873efea
avesheva <shevochkandrey@gmail.com>
2022-11-28 23:06:03 +0200

    Add library settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import * as path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'vue-fixed-size-accordion',
+      fileName: format => `vue-fixed-size-accordion.${ format }.js`
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
 })
